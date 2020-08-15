6f065e405d2dd0e3f299333984596e82b8bb4888
Nicklas Bocksberger <nicklas.bocksberger@gmail.com>
2020-08-15 12:28:42 +0200

    using map for charakter including

--- levels.cpp
+++ levels.cpp
@@ -1,10 +1,14 @@
 #include "levels.h"
 #include "inout.h"
 #include "gameManagement.h"
+#include "charakters.h"
 
 int startLevel(int currentLevel, LevelState* completedLevels){
     // create charakters
+    std::map *charakterMap = new std::map< std::string, Charakter>{}
 
+
+    
     // some gameplay
 
     // save progress
